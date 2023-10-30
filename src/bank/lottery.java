bc668336f27826a0bb15a91f8f718605ef09440d
EssosMc <@EssosMc>
2023-10-30 20:44:39 +0000

    Start of lottery rewrite, implements chance & logic for user input

--- src/bank/lottery.java
+++ src/bank/lottery.java
@@ -1,19 +1,37 @@
 package bank;
 
 /*
+- Implement math on winning
+- Implement math for returning money to user
+*/
+
+/*
  * @author Essos
  */
 public class lottery {
-  
-    double WinAmount; //Win amount
-    double Multipl; //Win multiplyer
-    int NoWin; //Win b4 taxes
     
+    double chance; // % chance of winning
     
-    void amount(int Amount) //Amount won from the lotter b4 taxes
-    {
-        NoWin = Amount;
-        WinAmount = Amount * Multipl;  //Calcualtion of lottery win after taxes ;(
+    void tickets(int Amount) { // Amount of tickets to be bought
+            
+        do {
+        if (Amount <= 5 && Amount >= 1) {
+            System.out.println("Confirmed you have purchased " + Amount + " lottery tickets");
+            chance = 0.25;
+        } else if (Amount <= 10 && Amount >= 6) {
+            System.out.println("Confirmed you have purchased " + Amount  + " lottery tickets");
+            chance = 0.50;
+        } else if (Amount <= 15 && Amount >= 11) {
+            System.out.println("Confirmed you have purchased " + Amount + " lottery tickets");
+            chance = 0.75;
+        } else if (Amount <= 20 && Amount >= 16) {
+            System.out.println("Confirmed you have purchased " + Amount + " lottery tickets");
+                    chance = 1.00;
+        } else {
+            System.out.println("Sorry you cannot buy that many tickets at once, you can only buy a minimum of 1 or a maximum of 20");
+        }
+    } while (Amount <= 0 || Amount >= 21);
+            
+            
+        }
     }
-    
-}
